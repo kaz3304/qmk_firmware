5f12bc70b6bb250508ca7fab3f0e1bf64f196101
cyril279 <cyril.x9@gmail.com>
2020-06-13 07:48:13 -0400

    mf68/../cyril: Switch to qmk-dfu bootloader

--- keyboards/40percentclub/mf68/keymaps/cyril/config.h
+++ keyboards/40percentclub/mf68/keymaps/cyril/config.h
@@ -1,2 +1,12 @@
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
+
+// set k64 [L_Ctrl] as dfu-escape key
+#define QMK_ESC_INPUT F5 // usually ROW
+#define QMK_ESC_OUTPUT D4 // usually COL
+
+// set k14 [Backspace] as reset key
+#define BOOTMAGIC_LITE_ROW 1
+#define BOOTMAGIC_LITE_COLUMN 4
+
+#define QMK_LED B0
