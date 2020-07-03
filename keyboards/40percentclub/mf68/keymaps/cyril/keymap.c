d2135d3cb2378c4ac76906b73d636ace3c99297f
cyril279 <cyril.x9@gmail.com>
2020-07-02 20:22:03 -0400

    mf68/../cyril: Moved alpha-cluster arrow keys to ijkl

--- keyboards/40percentclub/mf68/keymaps/cyril/keymap.c
+++ keyboards/40percentclub/mf68/keymaps/cyril/keymap.c
@@ -22,8 +22,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_MEDIA] = LAYOUT_68_ansi(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,           KC_VOLU, KC_PGUP,
-    KC_CAPS, KC_BTN3, _______, KC_UP,   _______, _______, _______, _______, _______, _______, KC_PSCR, _______, BL_STEP, _______,          KC_VOLD, KC_PGDN,
-    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,          _______,
+    KC_CAPS, KC_BTN3, _______, _______, _______, _______, _______, _______, KC_UP, _______,   KC_PSCR, _______, BL_STEP, _______,          KC_VOLD, KC_PGDN,
+    _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,          _______,
     _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_HOME, KC_END,  _______,                   _______,          KC_MUTE,
     _______, _______, _______,                            _______,                            _______, _______, _______,          KC_MPRV, KC_MPLY, KC_MNXT
   ),
