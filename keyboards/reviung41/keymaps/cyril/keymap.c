5e23de1cc6f4d6016e4a097194c65d782567f3d9
cyril279 <cyril.x9@gmail.com>
2021-01-27 12:39:35 -0500

    [keymap]reviung:cyril Add homerow mods
    Implemented per guidance of:
    https://precondition.github.io/home-row-mods

--- keyboards/reviung41/keymaps/cyril/keymap.c
+++ keyboards/reviung41/keymaps/cyril/keymap.c
@@ -33,10 +33,19 @@ enum custom_keycodes {
 #define ADJUST MO(_ADJUST)
 #define SPC_NAV LT(_NAVIG,KC_SPC)
 
+#define LGUI_A LGUI_T(KC_A)
+#define LALT_S LALT_T(KC_S)
+#define LSFT_D LSFT_T(KC_D)
+#define LCTL_F LCTL_T(KC_F)
+#define RCTL_J RCTL_T(KC_J)
+#define RSFT_K RSFT_T(KC_K)
+#define RALT_L LALT_T(KC_L)
+#define RGUI_SCLN RGUI_T(KC_SCLN)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,             KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    CTLESC,   KC_A,    KC_S,    KC_D,   KC_F,    KC_G,             KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_ESC, LGUI_A,  LALT_S,  LSFT_D, LCTL_F,    KC_G,             KC_H,   RCTL_J,  RSFT_K,  RALT_L,  RGUI_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,             KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
                                         KC_LALT, LOWER,   SPC_NAV, RAISE,  KC_LGUI
   ),
@@ -103,10 +112,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case CTLESC:
-        case SFTCAP:
-            return 75;
-        default:
-            return TAPPING_TERM;
+	case CTLESC:
+	case SFTCAP:
+	    return 75;
+	case LGUI_A:
+	case LALT_S:
+	case RALT_L:
+	case SPC_NAV:
+	    return 225;
+	default:
+	    return TAPPING_TERM;
     }
 }
